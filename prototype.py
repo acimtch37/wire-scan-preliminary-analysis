005e36b51406ac07463ab08fbb3eecb4654f7482
acimtch37 <38114469+acimtch37@users.noreply.github.com>
2018-04-11 19:19:03 -0500

    Add files via upload

---
+++ prototype.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 12:22:29 2018
+
+@author: Alec Mitchell
+"""
+
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+
+#with open('WM_1.csv','r') as csvfile:
+#    datareader = csv.reader(csvfile, delimiter=',')
+#    data = []
+#    for row in datareader:
+#        data.append(row)
+
+
+my_data = np.loadtxt('WM_1.csv', delimiter=',', skiprows=6)
+
+plt.pcolormesh(my_data,vmin=-5.5, vmax=-5.0)
+
+#y1=375
+#y2=430
+#y3=.5*(y1+y2)
+
+
